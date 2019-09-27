af92e8967f04f862577288ad91f8befd371143fa
kirselev <kirselev@gmail.com>
2019-09-27 19:39:13 +0300

    Updates

---
+++ precache-manifest.3e740662088d5a839532e24c5392810c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3885c18a86e326341203cd57321517f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ce5194a14b6dc506d4",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "14ce5194a14b6dc506d4",
+    "url": "/static/js/2.d685f09b.chunk.js"
+  },
+  {
+    "revision": "107bf0e0b2a8c83a6f3b",
+    "url": "/static/js/main.53263cab.chunk.js"
+  },
+  {
+    "revision": "ddbf46974b405d6daba7",
+    "url": "/static/js/runtime-main.596052c9.js"
+  }
+]);
